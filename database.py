ed68aafa8c1d17a86138bb61c5703a274f1c2a82
ashly-manuvel <113771609+ashly-manuvel@users.noreply.github.com>
2022-09-27 09:37:31 +0530

    Delete database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('users.db')
-conn.execute('''create table user( 
-email varchar(100),
-username varchar(100),
-roll_number int PRIMARY KEY,
-password varchar(100)
-);''')
-conn.close()
